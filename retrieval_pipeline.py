240bb6f8b8565260aa44f8f56a2445ec098ee45c
java-lang-goutam <25628844+java-lang-goutam@users.noreply.github.com>
2026-09-14 20:00:10 +0530

    Support chat history

--- retrieval_pipeline.py
+++ retrieval_pipeline.py
@@ -1,18 +1,21 @@
 from langchain_ollama import OllamaEmbeddings, ChatOllama
 from langchain_chroma import Chroma
-from langchain_core.messages import SystemMessage, HumanMessage
+from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
 
-import os, sys
+chat_history = []
+
 
 def get_embedding_model():
     return OllamaEmbeddings(model="mxbai-embed-large")
 
+
 def get_llm_model():
     return ChatOllama(model="llama3.1:8b")
 
+
 def get_llm_response(documents, query):
     model = get_llm_model()
-    
+
     llm_input = f"""Based on the following documents, please answer the question : {query}
     
     Documents : 
@@ -22,52 +25,76 @@ def get_llm_response(documents, query):
     """
     messages = [
         SystemMessage(content="You are helpful assistant"),
-        HumanMessage(content=llm_input)
+        HumanMessage(content=llm_input),
     ]
-    
+
     llm_response = model.invoke(messages)
-    
+
     return llm_response.content
 
-def main():
-    # Start ingestion pipeline
-    print("Starting RAG Retrieval pipeline")
-    persist_direcotry = "db/chroma_db"
-    
-    query = "tell me about Roadster in brief"
-    
-    # Embed query and pull the chunks
-    
-    # Step 1: Load embedding model
+
+def ask_question(query):
+
+    llm_model = get_llm_model()
     embedding_model = get_embedding_model()
-    
+
+    print(f"You asked : {query}")
+    search_question = query
+
+    if chat_history:
+        messages = (
+            [
+                SystemMessage(
+                    content="Given the chat history, rewrite the new question to be standalone and searchable. Just return the rewritten question."
+                )
+            ]
+            + chat_history
+            + [HumanMessage(content=f"New question: {query}")]
+        )
+
+        response = llm_model.invoke(messages)
+        search_question = response.content
+        print(f"Searching for : {search_question}")
+
+    persist_direcotry = "db/chroma_db"
+
     # Step 2 : Load vector store
     vector_store = Chroma(
         embedding_function=embedding_model,
         persist_directory=persist_direcotry,
-        collection_configuration={"hnsw:space": "cosine"}
+        collection_configuration={"hnsw:space": "cosine"},
     )
-    
+
     # Step 3: Configure retriever to load chunks
     vector_store_retriver = vector_store.as_retriever(
-        search_type = "similarity_score_threshold",
-        search_kwargs = {"k" : 3, "score_threshold" : 0.6}
+        search_type="similarity_score_threshold",
+        search_kwargs={"k": 5, "score_threshold": 0.3},
     )
-    
+
     # Step 4 : Load chunks
-    chunks = vector_store_retriver.invoke(query)
-    
-    # Step 5 : Print documents
-    # for i, doc in enumerate(chunks):
-    #     print("="*100)
-    #     print(f"{i+1}. {doc.metadata['source']}")
-    #     print(f"{doc.page_content}\n")
-    #     print("="*100)
-        
-    # Step 6 : Feed documents to model
-    model_response = get_llm_response(documents=chunks, query=query)
-    
-    print(model_response)
+    documents = vector_store_retriver.invoke(search_question)
+
+    # Step 5 : Get LLM Response
+    return get_llm_response(documents=documents, query=search_question)
+
+
+def main():
+    # Start ingestion pipeline
+    print("Ask me questions! type 'quit' to exit")
+
+    while True:
+        print("=" * 100)
+        question = input("\nYour question: ")
+        if question.lower() == "quit":
+            print("Good Bye!")
+            break
+
+        response = ask_question(question)
+        chat_history.append(HumanMessage(content=question))
+        chat_history.append(AIMessage(content=response))
+
+        print(response)
+
 
 if __name__ == "__main__":
     main()
